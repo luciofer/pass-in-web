7887c45bd337548debb6a0bbaf4bfeb487d0ac86
Lucio Araujo <contatolucioaraujo@gmail.com>
2024-04-07 20:53:55 -0300

    add backend

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# pass-in-web
+# Pass.in
+
+Project in development.
+
+## 👩🏾‍💻 Technologies
+
+-   React
+-   TypeScript
+-   Tailwind
+-   Node JS and fastfy
+-   PostgreSQL
+-   Prisma
+-   Docker
+-   Vercel
+-   Vite
+-   Figma
+
+## ✉ Contact
+
+contatolucioaraujo@gmail.com
+
+[Linkedin](https://www.linkedin.com/in/lucioaraujo30/)
--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -11,35 +11,96 @@ import { Table } from "./table/table"
 import { TableHeader } from "./table/table-header"
 import { TableCell } from "./table/table-cell"
 import { TableRow } from "./table/table-row"
-import { ChangeEvent, useState } from "react"
-import { attendees } from "../data/attendees"
+import { ChangeEvent, useEffect, useState } from "react"
 
 import dayjs from "dayjs"
 import relativeTive from "dayjs/plugin/relativeTime"
 
 dayjs.extend(relativeTive)
 
-export function AttendeeList() {
-    const [search, setSearch] = useState("")
-    const [page, setPage] = useState(1)
+interface Attendee {
+    id: string
+    name: string
+    email: string
+    checkedInAt: string | null
+    createdAt: string
+}
 
-    const totalPages = Math.ceil(attendees.length / 10)
+export function AttendeeList() {
+    const [search, setSearch] = useState(() => {
+        const url = new URL(window.location.toString())
+
+        if (url.searchParams.has("search")) {
+            return url.searchParams.get("search") ?? ""
+        }
+
+        return ""
+    })
+
+    const [page, setPage] = useState(() => {
+        const url = new URL(window.location.toString())
+
+        if (url.searchParams.has("page")) {
+            return Number(url.searchParams.get("page"))
+        }
+
+        return 1
+    })
+
+    const [attendees, setAttendees] = useState<Attendee[]>([])
+    const [total, setTotal] = useState(0)
+
+    const totalPages = Math.ceil(total / 10)
+
+    useEffect(() => {
+        const url = new URL(
+            "http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees"
+        )
+
+        url.searchParams.set("pageIndex", String(page - 1))
+
+        if (search.length > 0) {
+            url.searchParams.set("query", search)
+        }
+
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                setAttendees(data.attendees)
+                setTotal(data.total)
+            })
+    }, [page, search])
+
+    function setCurrentPage(page: number) {
+        const url = new URL(window.location.toString())
+        url.searchParams.set("page", String(page))
+        window.history.pushState({}, "", url)
+        setPage(page)
+    }
+
+    function setCurrentSearch(search: string) {
+        const url = new URL(window.location.toString())
+        url.searchParams.set("search", search)
+        window.history.pushState({}, "", url)
+        setSearch(search)
+    }
 
     function onSearchInputChange(event: ChangeEvent<HTMLInputElement>) {
-        setSearch(event.target.value)
+        setCurrentSearch(event.target.value)
+        setCurrentPage(1)
     }
 
     function goToNextPage() {
-        setPage(page + 1)
+        setCurrentPage(page + 1)
     }
     function goToPreviousPage() {
-        setPage(page - 1)
+        setCurrentPage(page - 1)
     }
     function goToLastPage() {
-        setPage(totalPages)
+        setCurrentPage(totalPages)
     }
     function goToFirstPage() {
-        setPage(1)
+        setCurrentPage(1)
     }
 
     return (
@@ -50,12 +111,11 @@ export function AttendeeList() {
                     <Search className="size-4 text-emerald-300" />
                     <input
                         onChange={onSearchInputChange}
-                        className="bg-transparent flex-1 outline-none p-0 text-sm border-0 ring-0"
+                        value={search}
+                        className="bg-transparent flex-1 outline-none p-0 text-sm border-0 focus:ring-0"
                         placeholder="Search participant"
                     />
                 </div>
-
-                {search}
             </div>
 
             <Table>
@@ -75,49 +135,53 @@ export function AttendeeList() {
                     </tr>
                 </thead>
                 <tbody>
-                    {attendees
-                        .slice((page - 1) * 10, page * 10)
-                        .map((attendee) => {
-                            return (
-                                <TableRow
-                                    key={attendee.id}
-                                    className="border-b border-white/10 hover:bg-white/5"
-                                >
-                                    <TableCell>
-                                        <input
-                                            type="checkbox"
-                                            className="size-4 bg-black/20 rounded border-white/10 "
-                                        />
-                                    </TableCell>
-                                    <TableCell>{attendee.id}</TableCell>
-                                    <TableCell>
-                                        <div className="flex flex-col gap-1">
-                                            <span className="font-semibold text-white">
-                                                {attendee.name}
-                                            </span>
-                                            <span>{attendee.email}</span>
-                                        </div>
-                                    </TableCell>
-                                    <TableCell>
-                                        {dayjs().to(attendee.createdAt)}
-                                    </TableCell>
-                                    <TableCell>
-                                        {dayjs().to(attendee.checkedAt)}
-                                    </TableCell>
-                                    <TableCell>
-                                        <IconButton transparent>
-                                            <MoreHorizontal className="size-4" />
-                                        </IconButton>
-                                    </TableCell>
-                                </TableRow>
-                            )
-                        })}
+                    {attendees.map((attendee) => {
+                        return (
+                            <TableRow
+                                key={attendee.id}
+                                className="border-b border-white/10 hover:bg-white/5"
+                            >
+                                <TableCell>
+                                    <input
+                                        type="checkbox"
+                                        className="size-4 bg-black/20 rounded border-white/10 "
+                                    />
+                                </TableCell>
+                                <TableCell>{attendee.id}</TableCell>
+                                <TableCell>
+                                    <div className="flex flex-col gap-1">
+                                        <span className="font-semibold text-white">
+                                            {attendee.name}
+                                        </span>
+                                        <span>{attendee.email}</span>
+                                    </div>
+                                </TableCell>
+                                <TableCell>
+                                    {dayjs().to(attendee.createdAt)}
+                                </TableCell>
+                                <TableCell>
+                                    {attendee.checkedInAt == null ? (
+                                        <span className="text-zinc-400">
+                                            Not checked in
+                                        </span>
+                                    ) : (
+                                        dayjs().to(attendee.checkedInAt)
+                                    )}
+                                </TableCell>
+                                <TableCell>
+                                    <IconButton transparent>
+                                        <MoreHorizontal className="size-4" />
+                                    </IconButton>
+                                </TableCell>
+                            </TableRow>
+                        )
+                    })}
                 </tbody>
 
                 <tfoot>
                     <tr>
                         <TableCell colSpan={3}>
-                            Showing 10 of {attendees.length}
+                            Showing {attendees.length} of {total}
                         </TableCell>
                         <TableCell className="text-right" colSpan={3}>
                             <div className="inline-flex gap-8 items-center">
